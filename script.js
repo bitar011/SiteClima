427619d74b2b55239540cbaf680e9f568dbb1998
bitar011 <victorbitarr@gmail.com>
2025-05-12 10:48:09 -0300

    Adiciona exibição de cidade oculta e animações na interface do clima

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const search = document.querySelector('.boxSearch button');
 const boxClima = document.querySelector('.boxClima');
 const climaDetails = document.querySelector('.climaDetails');
 const erro = document.querySelector('.notFound');
+const cityHide = document.querySelector('.city-hide');
 
 search.addEventListener('click', () => {
 
@@ -15,6 +16,7 @@ search.addEventListener('click', () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
         
         if(json.cod == '404') {
+            cityHide.textContent = city;
             container.style.height = '400px';
             boxClima.classList.remove('active');
             climaDetails.classList.remove('active');
@@ -22,10 +24,7 @@ search.addEventListener('click', () => {
             return;
         }
         
-        container.style.height = '555px';
-        boxClima.classList.add('active');
-        climaDetails.classList.add('active');
-        erro.classList.remove('active');
+        
         
         const image = document.querySelector('.boxClima img');
         const temperatura = document.querySelector('.boxClima .temperatura');
@@ -33,7 +32,23 @@ search.addEventListener('click', () => {
         const umidade = document.querySelector('.climaDetails .umidade span');
         const vento = document.querySelector('.climaDetails .vento span');
 
-        switch(json.weather[0].main) {
+        if(cityHide.textContent == city){
+            return;
+        }
+        else{
+            cityHide.textContent = city;
+
+            container.style.height = '555px';
+            container.classList.add('active');
+            boxClima.classList.add('active');
+            climaDetails.classList.add('active');
+            erro.classList.remove('active');
+
+            setTimeout(() => {
+                container.classList.remove('active');
+            }, 2500);
+
+            switch(json.weather[0].main) {
             case 'Clear':
                 image.src = 'assets/Sol.PNG';
                 break;
@@ -57,6 +72,52 @@ search.addEventListener('click', () => {
         descricao.innerHTML = `${json.weather[0].description}`;
         umidade.innerHTML = `${json.main.humidity}%`;
         vento.innerHTML = `${parseInt(json.wind.speed)}km/h`;
+
+        const climaDetails = document.querySelector('.climaDetails');
+        const infoUmidade = document.querySelector('.infoUmidade');
+        const infoVento = document.querySelector('.infoVento');
+
+        const elCloneClimaDetails = climaDetails.cloneNode(true);
+        const elCloneInfoUmidade = infoUmidade.cloneNode(true);
+        const elCloneInfoVento = infoVento.cloneNode(true);
+        
+        elCloneClimaDetails.id = 'cloneClimaDetails';
+        elCloneClimaDetails.classList.add('active-clone');
+
+        elCloneInfoUmidade.id = 'cloneInfoUmidade';
+        elCloneInfoUmidade.classList.add('active-clone');
+
+        elCloneInfoVento.id = 'cloneInfoVento';
+        elCloneInfoVento.classList.add('active-clone');
+
+        setTimeout(() => {
+            climaDetails.insertAdjacentElement("afterend", elCloneClimaDetails);
+            infoUmidade.insertAdjacentElement("afterend", elCloneInfoUmidade);
+            infoVento.insertAdjacentElement("afterend", elCloneInfoVento);
+        }, 2200);
+
+        const cloneClimaDetails = document.querySelectorAll('.climaDetails.active-clone');
+        const totalCloneClimaDetails = cloneClimaDetails.length;
+        const cloneClimaDetailsFirst = cloneClimaDetails[0];
+
+        const cloneInfoUmidade = document.querySelectorAll('.infoUmidade.active-clone');
+        const cloneInfoUmidadeFirst = cloneInfoUmidade[0];
+
+        const cloneInfoVento = document.querySelectorAll('.infoVento.active-clone');
+        const cloneInfoVentoFirst = cloneInfoVento[0];
+
+        if(totalCloneClimaDetails > 0){
+            cloneClimaDetailsFirst.classList.remove('active-clone');
+            cloneInfoUmidadeFirst.classList.remove('active-clone');
+            cloneInfoVentoFirst.classList.remove('active-clone');
+
+            setTimeout(() => {
+                cloneClimaDetailsFirst.remove();
+                cloneInfoUmidadeFirst.remove();
+                cloneInfoVentoFirst.remove();
+            }, 2200);
+        }
+    }
 
     });
 
